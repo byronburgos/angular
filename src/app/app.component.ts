ca057040b833840dbd34babde20e195b25681538
byronburgos <74152676+byronburgos@users.noreply.github.com>
2021-01-23 20:11:01 -0600

    varios cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-peliculas';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculasEnCines= [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }]
+      this.peliculasProximosEstrenos= [{
+        titulo: 'Matrix 4',
+        fechaLanzamiento: new Date(),
+        precio: 1230.99
+      },
+      {
+        titulo: 'Mandorian',
+        fechaLanzamiento: new Date('2021-11-14'),
+        precio: 300.99
+      },
+      {
+        titulo: 'Avenger 9',
+        fechaLanzamiento: new Date('2021-11-14'),
+        precio: 670.99
+      }]      
+    }, 500);
+  }
+
+
+  title = 'Listado de Peliculas';
+  peliculasEnCines;
+  peliculasProximosEstrenos;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
+import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPeliculasComponent,
+    ListadoGenericoComponent
   ],
   imports: [
     BrowserModule,
